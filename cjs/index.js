5a083da0eebccfe72bf7b4d1cb6527840414f8de
Paul Shorey <ps@artspaces.net>
2021-01-06 17:18:44 -0500

    redefined js class property causing babel issue

--- cjs/index.js
+++ cjs/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
-class horizontal_carousel {
-  carousel;
+class HorizontalCarousel {
   /*
    * To be called in React componentDidMount or similar,
    * after carousel div has been loaded into the DOM.
@@ -209,16 +208,16 @@ function debounce(callback, wait) {
  * </script>
  */
 if (typeof window === "object") {
-  window.horizontal_carousel = horizontal_carousel;
+  window.horizontal_carousel = HorizontalCarousel;
   window.horizontal_carousels = function (elements) {
     let refs = [];
     if (elements && elements.length) {
       for (let el of elements) {
-        refs.push(new horizontal_carousel(el));
+        refs.push(new HorizontalCarousel(el));
       }
     }
     return refs;
   };
 }
 
-module.exports = horizontal_carousel;
+module.exports = HorizontalCarousel;
