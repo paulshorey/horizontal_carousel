bf455c133faa378acefd695afef1a5ac8c7c6114
Paul Shorey <ps@artspaces.net>
2021-06-26 20:32:43 -0500

    fix and cleanup dist babel

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,6 @@ import { getBabelOutputPlugin } from '@rollup/plugin-babel';
 const config = {
   plugins: [getBabelOutputPlugin({
     presets: [['@babel/env', { modules: 'umd' }]],
-    plugins: ['@babel/plugin-proposal-class-properties']
   })]
 };
 
