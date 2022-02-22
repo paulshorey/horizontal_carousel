04b89fdfb89bbcf87c381abc0780295d05fa2bd4
Paul Shorey <web@artspaces.net>
2022-02-22 00:53:52 -0600

    will be used with lerna

--- src/index.js
+++ src/index.js
@@ -24,13 +24,17 @@
 
   function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
-  function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+  function _typeof(obj) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && "function" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }, _typeof(obj); }
+
+  function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+  function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
 
   function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
   function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-  var HorizontalCarousel =
+  var HorizontalCarousel = /*#__PURE__*/_createClass(
   /*
    * To be called in React componentDidMount or similar,
    * after carousel div has been loaded into the DOM.
@@ -212,13 +216,15 @@
    *    window.myCarousel.end()
    * </script>
    */
-  ;
+  );
   /*
    *
    * HELPER LIB:
    *
    */
 
+
+  _exports["default"] = HorizontalCarousel;
 
   function debounce(callback, wait) {
     var timeout = null;
@@ -285,7 +291,4 @@
       return refs;
     };
   }
-
-  var _default = HorizontalCarousel;
-  _exports["default"] = _default;
 });
